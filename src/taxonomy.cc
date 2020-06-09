c6b61fd25eeba29e94f9703b2aaf972dd00ab9aa
prashantpandey <prashant.prashn@gmail.com>
2020-06-09 08:04:17 -0700

    Adding taxon id and NCBI id both in the output.

--- src/taxonomy.cc
+++ src/taxonomy.cc
@@ -229,7 +229,7 @@ void Taxonomy::generatePrefixEncoding(const std::string &filename) const {
 			height--;
 		}
 																									
-		taxo_file << external_id  << '\t';
+		taxo_file << node.first << '\t' << external_id  << '\t';
 		for (uint32_t i = 0; i < encoding_len_bytes; i++)
 			taxo_file << (int)prefix_encoding[i] << ' ';
 		taxo_file << '\n';
